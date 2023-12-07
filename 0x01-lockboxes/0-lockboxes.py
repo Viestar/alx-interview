e41b8d5bedb55eb4474897de3f7afd0622366a62
Viestar <m.silverbuckson@gmail.com>
2023-12-07 05:38:23 +0300

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    # Initialize a set to keep track of visited boxes
+    visited = set()
+
+    # Define a DFS function to explore boxes
+    def dfs(box):
+        # Mark the current box as visited
+        visited.add(box)
+
+        # Explore keys in the current box
+        for key in boxes[box]:
+            # If the key opens an unvisited box, recursively explore that box
+            if key not in visited:
+                dfs(key)
+
+    # Start DFS from the first box
+    dfs(0)
+
+    # Check if all boxes have been visited
+    return len(visited) == len(boxes)
+
+# def canUnlockAll(boxes):
+#     """ Determines if all boxes can be opened """
+#     counter = 0
+#     for boxList in boxes:
+#         counter += 1
+#         for key in boxList:
+#             if int(key) == counter:
+#                 continue
+#             return False
+#         return True
