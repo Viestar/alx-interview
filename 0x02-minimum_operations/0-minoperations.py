fa1197f0508d8fcc3eac33bafd383df8c738a3d1
Viestar <m.silverbuckson@gmail.com>
2023-12-15 05:45:17 +0300

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Prototype: def minOperations(n)
+Returns an integer If n is impossible to achieve, return 0
+"""
+
+
+def minOperations(n):
+    """
+    In in exactly n H characters in the file, this returns the 
+    fewest number of operation need to result 
+    """
+    operations = 0
+    min_operations = 2
+    while n > 1:
+        while n % min_operations == 0:
+            operations += min_operations
+            n = n / min_operations
+        min_operations += 1
+    return operations
