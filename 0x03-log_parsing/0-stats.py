915d457f0eeea6c3b26ac2282de496a39cd945ca
Viestar <m.silverbuckson@gmail.com>
2023-12-22 05:31:27 +0300

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Module documentation for a script that reads stdin line by line and computes metrics:
+Module documentation for a script that reads stdin line by line
+and computes metrics:
 """
 import sys
+
 
 def print_statistics(total_size, status_codes):
     """ Prints status codes """
@@ -10,12 +12,14 @@ def print_statistics(total_size, status_codes):
     for code in sorted(status_codes):
         print(f"{code}: {status_codes[code]}")
 
+
 def parse_line(line):
     """ Returns parse lines """
     parts = line.split()
     if len(parts) >= 7 and parts[5].isdigit():
         return int(parts[5]), int(parts[6])
     return None
+
 
 def main():
     """ Program entry point """
@@ -28,7 +32,8 @@ def main():
             if data:
                 file_size, status_code = data
                 total_size += file_size
-                status_codes[status_code] = status_codes.get(status_code, 0) + 1
+                status_codes[status_code] = \
+                    status_codes.get(status_code, 0) + 1
 
             if i % 10 == 0:
                 print_statistics(total_size, status_codes)
@@ -37,6 +42,7 @@ def main():
         pass  # Handle keyboard interruption
 
     print_statistics(total_size, status_codes)
+
 
 if __name__ == "__main__":
     main()
